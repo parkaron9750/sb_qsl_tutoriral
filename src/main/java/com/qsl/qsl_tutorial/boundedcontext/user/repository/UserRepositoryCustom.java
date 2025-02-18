3b130a565ca9cb267018b721b3cd9d0347d35818
박아론 <me9750@naver.com>
2025-02-18 15:26:39 +0900

    jpaQueryFactory bean 추가

--- src/main/java/com/qsl/qsl_tutorial/boundedcontext/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/qsl/qsl_tutorial/boundedcontext/user/repository/UserRepositoryCustom.java
@@ -1,4 +1,7 @@
 package com.qsl.qsl_tutorial.boundedcontext.user.repository;
 
+import com.qsl.qsl_tutorial.boundedcontext.user.entity.SiteUser;
+
 public interface UserRepositoryCustom  {
+    SiteUser getQslUser(Long id);
 }
--- src/main/java/com/qsl/qsl_tutorial/boundedcontext/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/qsl/qsl_tutorial/boundedcontext/user/repository/UserRepositoryImpl.java
@@ -1,4 +1,21 @@
 package com.qsl.qsl_tutorial.boundedcontext.user.repository;
 
+import com.qsl.qsl_tutorial.boundedcontext.user.entity.SiteUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import static com.qsl.qsl_tutorial.boundedcontext.user.entity.QSiteUser.siteUser;
+
+@RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public SiteUser getQslUser(Long id) {
+        return queryFactory
+                .selectFrom(siteUser)
+                .where(siteUser.id.eq(id))
+                .fetchOne();
+    }
 }

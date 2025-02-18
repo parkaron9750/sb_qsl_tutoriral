77124048d913ddd1d27c0fa13efac0253e1a125b
박아론 <me9750@naver.com>
2025-02-18 14:31:42 +0900

    queryDSL 클래스 추가 UserRepositoryImpl 추가

--- src/main/java/com/qsl/qsl_tutorial/boundedcontext/user/repository/UserRepository.java
+++ src/main/java/com/qsl/qsl_tutorial/boundedcontext/user/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.qsl.qsl_tutorial.boundedcontext.user.repository;
 import com.qsl.qsl_tutorial.boundedcontext.user.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<SiteUser, Long> {
+public interface UserRepository extends JpaRepository<SiteUser, Long>, UserRepositoryCustom {
 
 }

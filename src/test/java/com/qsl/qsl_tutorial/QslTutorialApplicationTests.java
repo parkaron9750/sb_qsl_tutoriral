5bdd4cd0387849a6c98652e087acc6462007f0ef
박아론 <me9750@naver.com>
2025-02-18 14:14:30 +0900

    queryDSL 설정

--- src/test/java/com/qsl/qsl_tutorial/QslTutorialApplicationTests.java
+++ src/test/java/com/qsl/qsl_tutorial/QslTutorialApplicationTests.java
@@ -1,13 +1,27 @@
 package com.qsl.qsl_tutorial;
 
+import com.qsl.qsl_tutorial.boundedcontext.user.entity.SiteUser;
+import com.qsl.qsl_tutorial.boundedcontext.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class QslTutorialApplicationTests {
 
+	@Autowired
+	private UserRepository repository;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("회원 생성")
+	void createUserTest() {
+		SiteUser user1 = new SiteUser(null, "user1", "{noop}1234", "user1@qsl.com");
+		SiteUser user2 = new SiteUser(null, "user2", "{noop}1234", "user2@qsl.com");
+
+		repository.saveAll(Arrays.asList(user1, user2));
 	}
 
 }
